47610bbdea17ffbeb2eb63dbf98d14b8754e6e68
Premium07 <premsagarg23@gmail.com>
2025-02-10 18:03:57 +0545

    add error handling middleware for improved error responses

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabse from "./database/db.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
