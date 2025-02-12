dafefe3bfc1d0668ca78cd87e964a7c3fbb1f783
Premium07 <premsagarg23@gmail.com>
2025-02-12 16:36:21 +0545

    add authorization middleware and update user routes; include new environment variables

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getSingleUser, getUsers } from "../controllers/user.controller.js";
+import { authorized } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
-userRouter.get("/:id", getSingleUser);
+userRouter.get("/:id", authorized, getSingleUser);
 userRouter.post("/", (req, res) => res.send({ title: "create a user" }));
 userRouter.put("/:id", (req, res) => res.send({ title: "update user" }));
 userRouter.delete("/:id", (req, res) => res.send({ title: "delete all user" }));
